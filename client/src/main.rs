86cab38b5b53b54e0179cfe8f593d0d5c3f7c89d
Esteban Borai <estebanborai@gmail.com>
2020-10-25 22:23:20 -0300

    fmt: run clippy and fmt on client and server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{ErrorKind, Read, Write, stdin};
+use std::io::{stdin, ErrorKind, Read, Write};
 use std::net::TcpStream;
 use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
@@ -8,49 +8,53 @@ const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
 
 fn main() {
-  let mut client = TcpStream::connect(LOCAL).expect("Failed to connect");
-  client.set_nonblocking(true).expect("Failed to set not blocking");
+    let mut client = TcpStream::connect(LOCAL).expect("Failed to connect");
+    client
+        .set_nonblocking(true)
+        .expect("Failed to set not blocking");
 
-  let (sender, receiver) = mpsc::channel::<String>();
+    let (sender, receiver) = mpsc::channel::<String>();
 
-  thread::spawn(move || loop {
-    let mut buff = vec![0; MSG_SIZE];
-    match client.read_exact(&mut buff) {
-      Ok(_) => {
-        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-        println!("Received: {:?}", msg);
-      },
-      Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-      Err(_) => {
-        println!("Connection closed!");
-        break;
-      }
+    thread::spawn(move || loop {
+        let mut buff = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buff) {
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                println!("Received: {:?}", msg);
+            }
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("Connection closed!");
+                break;
+            }
+        }
+
+        match receiver.try_recv() {
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("Writting to socket failed");
+                println!("Message sent!");
+            }
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break,
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    });
+
+    println!("Write a message: ");
+
+    loop {
+        let mut buff = String::new();
+        stdin()
+            .read_line(&mut buff)
+            .expect("Reading from stdin failed!");
+
+        let msg = buff.trim().to_string();
+
+        if msg == ":quit" || sender.send(msg).is_err() {
+            break;
+        }
     }
-
-    match receiver.try_recv() {
-      Ok(msg) => {
-        let mut buff = msg.clone().into_bytes();
-        buff.resize(MSG_SIZE, 0);
-        client.write_all(&buff).expect("Writting to socket failed");
-        println!("Message sent!");
-      },
-      Err(TryRecvError::Empty) => (),
-      Err(TryRecvError::Disconnected) => break
-    }
-
-    thread::sleep(Duration::from_millis(100));
-  });
-
-  println!("Write a message: ");
-
-  loop {
-    let mut buff = String::new();
-    stdin().read_line(&mut buff).expect("Reading from stdin failed!");
-    
-    let msg = buff.trim().to_string();
-    
-    if msg == ":quit" || sender.send(msg).is_err() {
-      break;
-    }
-  }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -8,55 +8,62 @@ const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
 
 fn main() {
-  let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
-  server.set_nonblocking(true).expect("Failed to initialize non-blocking");
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
+    server
+        .set_nonblocking(true)
+        .expect("Failed to initialize non-blocking");
 
-  let mut clients = vec![];
-  let (sender, receiver) = mpsc::channel::<String>();
+    let mut clients = vec![];
+    let (sender, receiver) = mpsc::channel::<String>();
 
-  loop {
-    if let Ok((mut socket, addr)) = server.accept() {
-      println!("Client {} connected", addr);
+    loop {
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("Client {} connected", addr);
 
-      let sender = sender.clone();
-      clients.push(socket.try_clone().expect("Failed to clone client"));
+            let sender = sender.clone();
+            clients.push(socket.try_clone().expect("Failed to clone client"));
 
-      thread::spawn(move || loop {
-        let mut buff = vec![0; MSG_SIZE];
+            thread::spawn(move || loop {
+                let mut buff = vec![0; MSG_SIZE];
 
-        match socket.read_exact(&mut buff) {
-          Ok(_) => {
-            let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-            let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                match socket.read_exact(&mut buff) {
+                    Ok(_) => {
+                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
 
-            println!("{}: {:?}", addr, msg);
-            format!("Failed to send message to {}!", addr);
-            sender.send(msg).expect(&format!("Failed to send message to {}!", addr));
-          },
-          Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-          Err(_) => {
-            println!("Unexpected error closing connection with: {}", addr);
-            break;
-          }
+                        println!("{}: {:?}", addr, msg);
+                        format!("Failed to send message to {}!", addr);
+                        sender
+                            .send(msg)
+                            .unwrap_or_else(|_| panic!("Failed to send message to {}!", addr));
+                    }
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("Unexpected error closing connection with: {}", addr);
+                        break;
+                    }
+                }
+
+                sleep();
+            });
+        }
+
+        if let Ok(msg) = receiver.try_recv() {
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buff = msg.clone().into_bytes();
+
+                    buff.resize(MSG_SIZE, 0);
+                    client.write_all(&buff).map(|_| client).ok()
+                })
+                .collect::<Vec<_>>();
         }
 
         sleep();
-      });
     }
-
-    if let Ok(msg) = receiver.try_recv() {
-      clients = clients.into_iter().filter_map(|mut client| {
-        let mut buff = msg.clone().into_bytes();
-
-        buff.resize(MSG_SIZE, 0);
-        client.write_all(&buff).map(|_| client).ok()
-      }).collect::<Vec<_>>();
-    }
-
-    sleep();
-  }
 }
 
 fn sleep() {
-  thread::sleep(Duration::from_millis(100));
+    thread::sleep(Duration::from_millis(100));
 }
